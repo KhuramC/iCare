9d5bf5954f0accdb0928f036043cce6669b5a09d
Hipolito <80554014+hipolitoIV@users.noreply.github.com>
2024-10-27 16:58:39 -0500

    Add Patient view and Account view

---
+++ iCare/Models/TreatmentRecordExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iCare.Models
+{
+    public partial class TreatmentRecord
+    {
+        public List<PatientRecord> getAllPatients()
+        {
+            using (var context = new iCAREEntities())
+            {
+                var patients = context.PatientRecords.ToList();
+                return patients;
+            }
+        }
+    }
+}
