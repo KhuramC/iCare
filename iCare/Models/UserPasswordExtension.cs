cb094b8b524fd83b47cf52333769f2867d34fd12
Hipolito <80554014+hipolitoIV@users.noreply.github.com>
2024-10-26 19:48:51 -0500

    Created UserPasswordExtension.cs
    Contains getPassword method. We will change to encrypted password in the future.

---
+++ iCare/Models/UserPasswordExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iCare.Models
+{
+    public partial class UserPassword
+    {
+        // Return password of user specified by username.
+        public string getPassword(string username)
+        {
+            using (var context = new iCAREEntities1())
+            {
+                var userPassword = context.UserPasswords.FirstOrDefault(up => up.Username == username);
+                if (userPassword == null)
+                {
+                    return string.Empty;
+                }
+                return userPassword.Password;
+            }
+        }
+    }
+}
